8d1d4eb996656f703206175cdfbc5d37b35f4815
naleilan <leilan.n@gmail.com>
2024-01-24 15:43:24 +0300

    done cleanup function

--- src/App.jsx
+++ src/App.jsx
@@ -79,13 +79,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -98,7 +101,9 @@ export default function App() {
           setMovies(data.Search || []);
           setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -111,6 +116,11 @@ export default function App() {
       }
 
       fetchMovies();
+
+      //cleanup data fetching
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
